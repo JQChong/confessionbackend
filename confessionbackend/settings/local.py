833e4c916e19d6f97155af6a3b49a743b702ce6b
Chong Jing Quan <jingquan0302@gmail.com>
2021-01-07 21:18:02 +0800

    fix local settings

--- confessionbackend/settings/local.py
+++ confessionbackend/settings/local.py
@@ -18,6 +18,6 @@ CORS_ORIGIN_WHITELIST = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
     }
 }
